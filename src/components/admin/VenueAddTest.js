d706609abc937adc9f35be41602312faa425a1f6
Sijan Maharjan <112923124+Siz09@users.noreply.github.com>
2025-06-29 20:47:26 +0545

    Updated VenueAddTest.js

--- src/components/admin/VenueAddTest.js
+++ src/components/admin/VenueAddTest.js
@@ -93,6 +93,7 @@ const VenueAddTest = () => {
     <form onSubmit={handleSubmit}>
       <h2>Add New Venue</h2>
       {apiError && <div style={{ color: 'red', marginBottom: '10px' }}>{apiError}</div>}
+      }
       {Object.keys(formData).map((field) => (
         <div key={field}>
           <label htmlFor={field}>{field.replace(/([A-Z])/g, ' $1').toUpperCase()}:</label>
@@ -104,6 +105,7 @@ const VenueAddTest = () => {
             onChange={handleChange}
           />
           {errors[field] && <span style={{ color: 'red' }}>{errors[field]}</span>}
+          }
         </div>
       ))}
       <button type="submit" disabled={isSubmitting}>
